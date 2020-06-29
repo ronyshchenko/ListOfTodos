cbe9db2ce9ef3ff0cbd9a0240db0032bd9e0d257
ronyshchenko <roma.onyshchenko@gmail.com>
2020-06-29 17:16:08 +0300

    Updates

---
+++ precache-manifest.f3ba2059d476a80424e3996c961b8ccc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a13df2fffcb6f3b8f32d89219827f123",
+    "url": "/listOfTodos/index.html"
+  },
+  {
+    "revision": "5e16dacfd2760aec2657",
+    "url": "/listOfTodos/static/css/main.9dde7378.chunk.css"
+  },
+  {
+    "revision": "9d227c3db4761dab894a",
+    "url": "/listOfTodos/static/js/2.ba6d3c10.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/listOfTodos/static/js/2.ba6d3c10.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e16dacfd2760aec2657",
+    "url": "/listOfTodos/static/js/main.f37089e6.chunk.js"
+  },
+  {
+    "revision": "6922a25990be2db51345",
+    "url": "/listOfTodos/static/js/runtime-main.6b028474.js"
+  }
+]);
